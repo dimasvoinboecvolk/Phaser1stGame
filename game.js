c89c0629d161f47511009a9e7fef94a9cff4d326
dimasvoinboecvolk <158043890+dimasvoinboecvolk@users.noreply.github.com>
2024-02-07 15:08:24 +0200

    поміняв місце вилету бомб

--- game.js
+++ game.js
@@ -128,19 +128,19 @@ function update() {
 function collectStar(player, star) {
     star.disableBody(true, true);
 
-    score += 10;
+    score += 1;
     scoreText.setText('Score: ' + score);
 
     if (stars.countActive(true) === 0) {
         stars.children.iterate(function (child) {
 
-            child.enableBody(true, child.x, 0, true, true);
+            child.enableBody(true, child.x, 16, true, true);
 
         });
 
         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
-        var bomb = bombs.create(x, 16, 'bomb');
+        var bomb = bombs.create(x, 3, 'bomb');
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
